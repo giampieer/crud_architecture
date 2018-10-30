05f614e60b0451abe8025758d70c7645282e28c2
unknown <giampieer24@gmail.com>
2018-10-30 14:05:36 -0500

    orden de carpetas a mvvm

--- app/src/main/java/mvvm/com/login/view/MainActivity.java
+++ app/src/main/java/mvvm/com/login/view/MainActivity.java
@@ -1,6 +1,5 @@
-package mvvm.com.login.activity;
+package mvvm.com.login.view;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
